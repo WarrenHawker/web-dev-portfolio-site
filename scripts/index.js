0a11cdc9829d8852f336a15fe1ecb73218ffd098
WanderingMist <hawker.warren@gmail.com>
2022-07-12 00:52:51 +0100

    GOT SMOOTH TRANSITIONS ON THEMES WORKING PERFECTLY!!!

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,30 @@
 const checkbox = document.getElementById('checkbox');
+const bgSummer = document.getElementsByClassName('background-colors-summer')[0];
+const bgWinter = document.getElementsByClassName('background-colors-winter')[0];
+const mainElement = document.getElementsByTagName('main')[0];
+let pageHeight = mainElement.getBoundingClientRect();
 
 checkbox.addEventListener('change', () => {
-	console.log(checkbox.checked);
 	if (checkbox.checked) {
 		document.body.classList.add('theme-winter');
 		document.body.classList.remove('theme-summer');
+		bgSummer.style.opacity = 0;
+		bgWinter.style.opacity = 1;
 	} else if (!checkbox.checked) {
 		document.body.classList.add('theme-summer');
 		document.body.classList.remove('theme-winter');
+		bgSummer.style.opacity = 1;
+		bgWinter.style.opacity = 0;
 	}
+});
+
+//set initial background height on page load
+bgSummer.style.height = `${pageHeight.height}px`;
+bgWinter.style.height = `${pageHeight.height}px`;
+
+window.addEventListener('resize', () => {
+	//change background height when window is resized
+	pageHeight = mainElement.getBoundingClientRect();
+	bgSummer.style.height = `${pageHeight.height}px`;
+	bgWinter.style.height = `${pageHeight.height}px`;
 });
