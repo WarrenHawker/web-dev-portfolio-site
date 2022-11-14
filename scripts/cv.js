aed34c3391bf59ef6c83fada1c8eda4e41ebbbe0
WarrenHawker <hawker.warren@gmail.com>
2022-11-14 09:37:19 +0000

    added cv overlay

---
+++ scripts/cv.js
@@ -0,0 +1,11 @@
+const overlay = document.getElementById('cv-overlay');
+const showCVButton = document.getElementById('btn-show-cv');
+const closeCVButton = document.getElementById('cv-close');
+
+showCVButton.addEventListener('click', () => {
+  overlay.classList.add('active');
+});
+
+closeCVButton.addEventListener('click', () => {
+  overlay.classList.remove('active');
+});
